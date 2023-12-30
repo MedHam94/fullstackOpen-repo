fb36e4ce36e98f75d19f466f82fad58f3f685e8e
MedHam <hammas.m.mh@gmail.com>
2023-12-30 17:53:46 +0100

    fix the phonebook step 2 errors and finish the phonebook step 3

--- part2/thePhoneBook/src/App.jsx
+++ part2/thePhoneBook/src/App.jsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
 
 const App = () => {
-
   // State variable for managing persons array and newName
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
-  const [newName, setNewName] = useState("");
+  const [persons, setPersons] = useState([{ name: "Arto Hellas", number: '040-1234567' }]);
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
 
   // Event handler for add a new name to persons array
   const addName = (event) => {
@@ -13,34 +13,45 @@ const App = () => {
     console.log("button Clicked", event.target);
 
     // Create a new name object and update persons state
-    const name = {name:newName} 
-    console.log('this is the name', name.name);
-    
-    for(let i =0; i<persons.length; i++){
-      if(persons[i].name === name.name){
-        alert(`${persons[i].name} is already added to phonebook`)
-        break
-      }else{
-        setPersons(persons.concat(name))
+    const person = [{ name: newName, number: newNumber}]
+    console.log("this is the name", person[0].name, person[0].number);
+
+    let nameOrNumberAlreadyExists = false;
+
+    for (let i = 0; i < persons.length; i++) {
+      if (persons[i].name === person[0].name  || persons[i].number === person[0].number) {
+        alert(`${persons[i].name} or ${persons[i].number} is already added to phonebook`);
+        nameOrNumberAlreadyExists = true;
+        break;
       }
     }
-    
+    if (!nameOrNumberAlreadyExists) {
+      console.log('added',persons.concat(person));
+      setPersons(persons.concat(person));
+    }
   };
 
   // Event handler for updating newName state as user types
   const handleNameChange = (event) => {
-    console.log(event.target.value);
+    console.log('this is input hundler',event.target.value);
     setNewName(event.target.value);
+  };
+  const handleNumberChange = (event) => {
+    console.log('this is input hundler',event.target.value);
+    setNewNumber(event.target.value);
   };
 
   console.log(persons);
-  
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addName} >
+      <form onSubmit={addName}>
         <div>
-          name: <input value={newName}  onChange={handleNameChange}/>
+          name: <input value={newName.name} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber.number} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
@@ -48,9 +59,10 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map((el,i)=><li key={el.name}>{el.name}</li>)}
+        {persons.map((el) => (
+          <li key={el.name}>{el.name} {el.number}</li>
+        ))}
       </ul>
-      
     </div>
   );
 };
