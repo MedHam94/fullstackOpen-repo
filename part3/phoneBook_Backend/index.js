86f03105f1856fedb2925e3790b214a1e9edb51d
MedHam <hammas.m.mh@gmail.com>
2024-01-22 16:33:29 +0100

    Phonebook backend step7

--- part3/phoneBook_Backend/index.js
+++ part3/phoneBook_Backend/index.js
@@ -1,8 +1,12 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 const PORT = 3001
 
 app.use(express.json())
+
+app.use(morgan(':method :url :status :response-time ms'));
+
 
 let persons = [
     {
