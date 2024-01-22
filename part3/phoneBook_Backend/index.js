cdf60036765a71ad4aa6bcc685be81674e200138
MedHam <hammas.m.mh@gmail.com>
2024-01-22 15:26:56 +0100

    Phonebook backend step6

--- part3/phoneBook_Backend/index.js
+++ part3/phoneBook_Backend/index.js
@@ -5,31 +5,41 @@ const PORT = 3001
 app.use(express.json())
 
 let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
+    {
+        "id": 1,
+        "name": "Arto Hellas",
+        "number": "040-123456"
     },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
+    {
+        "id": 2,
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
     },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
+    {
+        "id": 3,
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
     },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
+    {
+        "id": 4,
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
     }
 ]
 
-const generateID = ()=>{
-    return Math.floor(Math.random().toFixed(2)*100)
+const generateID = () => {
+    return Math.floor(Math.random().toFixed(2) * 100)
 }
+
+const findDuplicate = (array, obj) => {
+
+    let names = Object.entries(array).map(el => el[1].name)
+    return names.includes(obj.name)
+
+}
+// console.log(findDuplicate(persons));
+
+
 
 app.get('/', (req, res) => res.send('<h1>Hello World</h1>'))
 
@@ -37,14 +47,14 @@ app.get('/api/persons/', (req, res) => {
     res.json(persons)
 })
 
-app.get('/api/persons/:id', (req,res)=>{
+app.get('/api/persons/:id', (req, res) => {
     console.log(typeof (+req.params.id));
     const id = +req.params.id
-    const person = persons.find(person=> person.id === id)
+    const person = persons.find(person => person.id === id)
 
-    if(person){
+    if (person) {
         res.json(person)
-    }else{
+    } else {
         res.status(404).end()
     }
 })
@@ -55,20 +65,27 @@ app.get('/info', (req, res) => {
     res.send(`<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p>`)
 })
 
-app.post('/api/persons', (req,res)=>{
-    console.log(req.body);
+app.post('/api/persons', (req, res) => {
+    
     const person = {
+        id: generateID(),
         name: 'med',
-        number:'456123',
-        id:generateID()
+        number: '456123'
     }
 
+    if (!person.name || !person.number) {
+        res.status(404).json({ error: 'The name or number is missing' })
+        return;
+    } else if (findDuplicate(persons, person)) {
+         res.status(404).json({error:'The name already exists in the phonebook'})
+        return;
+    }
     persons = persons.concat(person)
     res.json(person)
 
 })
 
-app.delete('/api/persons/:id', (req,res)=>{
+app.delete('/api/persons/:id', (req, res) => {
     const id = +req.params.id
     console.log(id);
     persons = persons.filter(person => person.id !== id)
@@ -76,6 +93,7 @@ app.delete('/api/persons/:id', (req,res)=>{
     res.status(204).end()
 })
 
+
 app.listen(PORT, () => {
-    console.log(`The app in running in ${PORT}`);
+    console.log(`The app in running in http://localhost:${PORT}`);
 })
