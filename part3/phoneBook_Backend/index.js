25e2022ac3b3a1106ec9957420462e81c6a79aa8
MedHam <hammas.m.mh@gmail.com>
2024-01-23 16:54:37 +0100

    importing

--- part3/phoneBook_Backend/index.js
+++ part3/phoneBook_Backend/index.js
@@ -1,13 +1,9 @@
 const express = require('express')
-const morgan = require('morgan')
 const app = express()
-const PORT = process.env.PORT || 3001
+const PORT = 3001
 
 app.use(express.json())
-
-app.use(morgan(':method :url :status :response-time ms'));
-
-
+app.use(express.static('dist'))
 let persons = [
     {
         "id": 1,
@@ -31,19 +27,6 @@ let persons = [
     }
 ]
 
-const generateID = () => {
-    return Math.floor(Math.random().toFixed(2) * 100)
-}
-
-const findDuplicate = (array, obj) => {
-
-    let names = Object.entries(array).map(el => el[1].name)
-    return names.includes(obj.name)
-
-}
-// console.log(findDuplicate(persons));
-
-
 
 app.get('/', (req, res) => res.send('<h1>Hello World</h1>'))
 
@@ -51,16 +34,9 @@ app.get('/api/persons/', (req, res) => {
     res.json(persons)
 })
 
-app.get('/api/persons/:id', (req, res) => {
+app.get('/api/persons/:id', (req,res)=>{
     console.log(typeof (+req.params.id));
     const id = +req.params.id
-    const person = persons.find(person => person.id === id)
-
-    if (person) {
-        res.json(person)
-    } else {
-        res.status(404).end()
-    }
 })
 
 app.get('/info', (req, res) => {
@@ -69,35 +45,6 @@ app.get('/info', (req, res) => {
     res.send(`<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p>`)
 })
 
-app.post('/api/persons', (req, res) => {
-    
-    const person = {
-        id: generateID(),
-        name: 'med',
-        number: '456123'
-    }
-
-    if (!person.name || !person.number) {
-        res.status(404).json({ error: 'The name or number is missing' })
-        return;
-    } else if (findDuplicate(persons, person)) {
-         res.status(404).json({error:'The name already exists in the phonebook'})
-        return;
-    }
-    persons = persons.concat(person)
-    res.json(person)
-
-})
-
-app.delete('/api/persons/:id', (req, res) => {
-    const id = +req.params.id
-    console.log(id);
-    persons = persons.filter(person => person.id !== id)
-    console.log(persons);
-    res.status(204).end()
-})
-
-
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+    console.log(`The app in running in ${PORT}`);
 })
