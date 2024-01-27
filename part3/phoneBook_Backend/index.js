b7262b5b6b3440beac33c87d5d2b8987f003aaed
MedHam <hammas.m.mh@gmail.com>
2024-01-27 11:57:09 +0100

    fix the client side code, after fixing in part 2 code , and send new dist

--- part3/phoneBook_Backend/index.js
+++ part3/phoneBook_Backend/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
+const cors = require("cors")
 const PORT = 3001
 
 app.use(express.json())
 app.use(express.static('dist'))
+app.use(cors())
 let persons = [
     {
         "id": 1,
@@ -27,16 +29,36 @@ let persons = [
     }
 ]
 
+const generateID = Math.floor(Math.random().toFixed(2) * 100)
 
-app.get('/', (req, res) => res.sendFile('./dist/index.html'))
+const findDuplicate = (array, obj) => {
+
+    let names = Object.entries(array).map(el => el[1].name)
+    return names.includes(obj.name)
+
+}
+
+
+
+
+app.get('/', (req, res) => {
+    res.render('./dist/')
+})
 
 app.get('/api/persons/', (req, res) => {
     res.json(persons)
 })
 
-app.get('/api/persons/:id', (req,res)=>{
+app.get('/api/persons/:id', (req, res) => {
     console.log(typeof (+req.params.id));
     const id = +req.params.id
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
 })
 
 app.get('/info', (req, res) => {
@@ -45,6 +67,37 @@ app.get('/info', (req, res) => {
     res.send(`<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p>`)
 })
 
-app.listen(PORT, () => {
-    console.log(`The app in running in ${PORT}`);
+app.post('/api/persons', (req, res) => {
+
+    const person = {
+        id: generateID,
+        name: req.body.name,
+        number: req.body.number
+    }
+
+    if (!person.name || !person.number) {
+        res.status(404).json({ error: 'The name or number is missing' })
+        return;
+    } else if (findDuplicate(persons, person)) {
+        res.status(404).json({ error: 'The name already exists in the phonebook' })
+        return;
+    } else {
+        persons = persons.concat(person)
+        res.json(person)
+    }
+
+
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = +req.params.id
+    console.log(id);
+    persons = persons.filter(person => person.id !== id)
+    console.log(persons);
+    res.status(204).end()
+})
+
+
+app.listen(3001, () => {
+    console.log(`The app in running in http://localhost:${PORT}`);
 })
