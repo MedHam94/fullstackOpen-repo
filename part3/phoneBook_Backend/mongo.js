d129ab0112bf0c4695d77980f8ca25ebae2d1164
MedHam <hammas.m.mh@gmail.com>
2024-01-28 17:28:24 +0100

    3.12: Command-line database

---
+++ part3/phoneBook_Backend/mongo.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+
+if(process.argv.length<3){
+    console.log('give password as argument');
+    process.exit()
+}
+
+const password = process.argv[2]
+
+
+const url = `mongodb+srv://medham:${password}@cluster0.qwi7tef.mongodb.net/`
+
+mongoose.connect(url)
+
+const personsSchema = new mongoose.Schema({
+    name:String,
+    number: String
+})
+
+const Person = mongoose.model('Person', personsSchema)
+
+const person = new Person({
+    name:process.argv[3],
+    number: process.argv[4]
+})
+
+person
+    .save()
+    .then(result=> {
+        console.log(`added ${person.name} number ${person.number} to phonebook`)
+        mongoose.connection.close()
+    })
+
